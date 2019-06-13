aa93e41373d6826a89079962b7ea07fc69aa1b41
Milene Toazza <mitoe@live.com>
2019-06-13 00:36:41 -0300

    Function shuffle

--- src/modules/gallery.js
+++ src/modules/gallery.js
@@ -1,3 +1,24 @@
-export default function () {
-  
+function* shuffle(array) {
+  var i = array.length;
+  while (i--) {
+    yield array.splice(Math.floor(Math.random() * (i+1)), 1)[0];
+  }
 }
+
+var ranNums = shuffle([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]);
+
+const images = [];
+
+for (let i = 0; i < 20; i++) {
+  images.push(
+    `
+    <picture class="image">
+      <img src="/images/image-${ranNums.next().value}.jpg" alt="Grid Gallery Image">
+    </picture>
+    `
+  )
+}
+
+console.log(images.join(''))
+
+document.getElementById('gallery').innerHTML = images.join('');
